3983c448327d7637ff99a7ebffe45e348ce061d6
Javier Fernandes <javierjfernandezd@gmail.com>
2017-08-16 10:39:20 -0400

    Add Drawer resources

---
+++ MvvmCross-AndroidDrawer.Android/SplashScreen.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content.PM;
+using MvvmCross.Droid.Views;
+
+namespace MvvmCrossAndroidDrawer.Android
+{
+    [Activity(
+        Label = "Example.Droid"
+        , MainLauncher = true
+        , Icon = "@drawable/icon"
+        , Theme = "@style/AppTheme.Splash"
+        , NoHistory = true
+        , ScreenOrientation = ScreenOrientation.Portrait)]
+    public class SplashScreen : MvxSplashScreenActivity
+    {
+        public SplashScreen()
+            : base(Resource.Layout.splash_screen)
+        {
+        }
+    }
+}
